e77cd95c672bf97e1362226c0694ec8dd393f98b
Caleb Bayles <33640719+cbayles1@users.noreply.github.com>
2024-03-26 09:45:55 -0500

    Add files via upload

--- dataFormatting.py
+++ dataFormatting.py
@@ -1,26 +1,5 @@
 import os, random
 
-def cropLengths(pos, neg):
-    sumLineLen = 0
-    # For now, I'm just using the avg length of positive lines to crop both positive and negative, to avoid empty positive outputs.
-    # I could use the avg length to split the negative lines up into chunks if needed, but that would skew the data heavily.
-    for line in pos: sumLineLen += len(line)
-    avgLineLen = int(sumLineLen / len(pos))
-
-    pos1 = [line for line in pos if len(line) >= avgLineLen]
-    neg1 = [line for line in neg if len(line) >= avgLineLen]
-    for i, line in enumerate(pos1):
-        pos1[i] = line[0:avgLineLen]
-    for i, line in enumerate(neg1):
-        neg1[i] = line[0:avgLineLen]
-    return pos1, neg1
-
-def exportAtgcToFile(atgc, filepath):
-    os.makedirs(os.path.dirname(filepath), exist_ok=True)
-    with open(filepath, 'w') as f:
-        for nuc in atgc:
-            f.write(nuc + "\n")
-
 def removeHeaderLine(inFile):
     arr = []
     with open(inFile, 'r') as f:
@@ -33,22 +12,48 @@ def removeHeaderLine(inFile):
             arr.append(nucleotide)
     return arr
 
+def getAvgPosLength(positives):
+    sumLineLen = 0
+    sumFileLen = 0
+    for x in positives:
+        sumFileLen += len(x)
+        for line in x:
+            sumLineLen += len(line)
+    return int(sumLineLen / sumFileLen)
+
+def cropLengths(x, avgLineLen):
+    x1 = [line for line in x if len(line) >= avgLineLen]
+    for i, line in enumerate(x1):
+        x1[i] = line[0:avgLineLen]
+    return x1
+
+def exportAtgcToFile(atgc, filepath):
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'w') as f:
+        for nuc in atgc:
+            f.write(nuc + "\n")
+
 def main(posInDir, posOutDir, negInDir, negOutDir):
-    smallerInDir = posInDir
-    if len(negInDir) < len(posInDir):
-        smallerInDir = negInDir
-    for inFilename in os.listdir(smallerInDir):
+    positives = []
+    negatives = []
+    
+    for inFilename in os.listdir(posInDir):
         if inFilename.startswith(".ipynb_checkpoints"): continue
-        
-        x = removeHeaderLine(posInDir + inFilename)
-        y = removeHeaderLine(negInDir + inFilename)
-        x, y = cropLengths(x, y)
+        positives.append(removeHeaderLine(posInDir + inFilename))
+        negatives.append(removeHeaderLine(negInDir + inFilename))
+
+    avgLineLen = getAvgPosLength(positives)
+    
+    for i, x in enumerate(positives):
+        y = negatives[i]
+        x = cropLengths(x, avgLineLen)
+        y = cropLengths(y, avgLineLen)
         x = [nuc.upper() for nuc in x] # capitalize
         y = [nuc.upper() for nuc in y] # capitalize
         random.shuffle(x)
         random.shuffle(y)
-        exportAtgcToFile(x, posOutDir + inFilename)
-        exportAtgcToFile(y, negOutDir + inFilename)
+        exportAtgcToFile(x, posOutDir + str(i + 1) + ".txt")
+        exportAtgcToFile(y, negOutDir + str(i + 1) + ".txt")
 
 if __name__ == "__main__":
     main("./data/atgc/positives/", "./data/positives/", "./data/atgc/negatives/", "./data/negatives/")
